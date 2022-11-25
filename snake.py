50392e4c015ed44df228219abf9dc66a57bf0a27
Dracula` <113742499+BelalDracula1@users.noreply.github.com>
2022-11-25 02:00:36 +0200

    Update snake.py

--- snake.py
+++ snake.py
@@ -14,7 +14,7 @@ orange = (255, 165, 0)
 width, height = 600, 400
 
 game_display = pygame.display.set_mode((width, height))
-pygame.display.set_caption("NeuralNine Snake Game")
+pygame.display.set_caption("Snake Game")
 
 clock = pygame.time.Clock()
 
